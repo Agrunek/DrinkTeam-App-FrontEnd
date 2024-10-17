da87701fef8815c8e2f365b63b138a0e898d95c2
Adrian Antczak <adek24680@gmail.com>
2024-10-18 00:50:22 +0200

    Create Search View

---
+++ apps/DrinkTeamWeb/src/views/Search/SearchView.tsx
@@ -0,0 +1,171 @@
+import type { MD3Colors } from 'react-native-paper/lib/typescript/types';
+
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { useTheme, Text, TextInput, Icon } from 'react-native-paper';
+
+interface ItemData {
+  id: number;
+  name: string;
+  rating: number;
+  difficulty: string;
+  duration: string;
+}
+
+const DATA: ItemData[] = [
+  {
+    id: 1,
+    name: 'Nalewka z jabłka',
+    rating: 5,
+    difficulty: 'Łatwe',
+    duration: '2 miesiące',
+  },
+  {
+    id: 2,
+    name: 'Nalewka z babka',
+    rating: 5,
+    difficulty: 'Łatwe',
+    duration: '2 miesiące',
+  },
+  {
+    id: 3,
+    name: 'Nalewka z dziadka',
+    rating: 5,
+    difficulty: 'Łatwe',
+    duration: '2 miesiące',
+  },
+  {
+    id: 4,
+    name: 'Nalewka z kawka',
+    rating: 5,
+    difficulty: 'Łatwe',
+    duration: '2 miesiące',
+  },
+  {
+    id: 5,
+    name: 'Nalewka z klatka',
+    rating: 5,
+    difficulty: 'Łatwe',
+    duration: '2 miesiące',
+  },
+];
+
+const SearchView = () => {
+  const { colors } = useTheme();
+
+  const style = styles(colors);
+
+  const Item = ({ name, rating, difficulty, duration }: ItemData) => {
+    return (
+      <View style={style.listItemContainer}>
+        <View style={style.listItemImage}></View>
+        <View style={style.listItemDescription}>
+          <Text style={style.listItemName}>{name}</Text>
+          <View style={style.listItemRating}>
+            {[...Array(rating).keys()].map((id) => (
+              <View
+                key={id}
+                style={{ backgroundColor: '#FFE600', width: 10, height: 10 }}
+              />
+            ))}
+          </View>
+          <Text style={style.listItemDifficulty}>{difficulty}</Text>
+          <Text style={style.listItemDuration}>Czas trwania: {duration}</Text>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={style.container}>
+      <View style={style.header}>
+        <Text style={style.headerText}>Szukaj</Text>
+        <TextInput
+          mode="outlined"
+          left={<Icon source="magnify" size={14} />}
+          outlineStyle={style.headerSearchOutline}
+          style={style.headerSearch}
+          textColor={colors.onBackground}
+          placeholder="Search"
+          placeholderTextColor="#9586A8"
+        />
+      </View>
+      <FlatList
+        style={style.listContainer}
+        data={DATA}
+        renderItem={({ item }) => <Item {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+      ></FlatList>
+    </View>
+  );
+};
+
+const styles = (colors: MD3Colors) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.background,
+    },
+    header: {
+      marginHorizontal: 20,
+      marginTop: 40,
+      marginBottom: 30,
+      gap: 20,
+    },
+    headerText: {
+      fontSize: 30,
+      color: colors.onBackground,
+    },
+    headerSearch: {
+      backgroundColor: '#23232C',
+      paddingHorizontal: 10,
+      fontSize: 14,
+      height: 50,
+    },
+    headerSearchOutline: {
+      borderRadius: 99999,
+      borderColor: '#9586A8',
+    },
+    listContainer: {
+      padding: 20,
+      paddingTop: 0,
+    },
+    listItemContainer: {
+      borderWidth: 2,
+      borderColor: '#9586A8',
+      borderRadius: 20,
+      backgroundColor: '#23232C',
+      flexDirection: 'row',
+      overflow: 'hidden',
+      marginVertical: 10,
+    },
+    listItemImage: {
+      backgroundColor: 'white',
+      flex: 2,
+    },
+    listItemDescription: {
+      flex: 3,
+      paddingHorizontal: 10,
+      paddingVertical: 10,
+      gap: 5,
+    },
+    listItemName: {
+      color: '#9586A8',
+      fontSize: 16,
+    },
+    listItemRating: {
+      flexDirection: 'row',
+      gap: 10,
+      marginVertical: 5,
+    },
+    listItemDifficulty: {
+      color: '#0BCE83',
+    },
+    listItemDuration: {
+      color: '#FFFFFF',
+      fontSize: 12,
+    },
+  });
+};
+
+export default SearchView;

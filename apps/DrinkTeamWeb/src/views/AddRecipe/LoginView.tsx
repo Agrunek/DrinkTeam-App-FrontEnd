809938a20d50190953b1f8e950f922da236fed16
Theo <theo.deschaeck@gmail.com>
2024-12-11 17:18:54 +0100

    Add create button

---
+++ apps/DrinkTeamWeb/src/views/AddRecipe/LoginView.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import { TextInput, Button, useTheme, Text } from 'react-native-paper';
+import { useTest } from '../../middleware/queries';
+import { useNavigation } from '@react-navigation/native';
+
+const LoginView = () => {
+  const theme = useTheme();
+  const { colors } = theme;
+  const [testStr, setTestStr] = useState('');
+  const { data } = useTest(testStr);
+  const navigation = useNavigation();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.primary,
+    },
+    topSection: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingVertical: 40,
+      borderBottomLeftRadius: 30,
+      borderBottomRightRadius: 30,
+    },
+    headerText: {
+      fontSize: 36,
+      color: colors.onPrimary,
+      fontWeight: 'bold',
+    },
+    taglineText: {
+      fontSize: 16,
+      color: colors.onPrimary,
+      marginTop: 10,
+    },
+    middleSection: {
+      flex: 3,
+      paddingVertical: 40,
+      paddingHorizontal: 20,
+      marginTop: -20,
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      alignItems: 'center',
+      backgroundColor: colors.background,
+    },
+    logoText: {
+      fontSize: 34,
+      color: colors.onBackground,
+      marginBottom: 20,
+    },
+    loginText: {
+      color: colors.primary,
+      fontSize: 24,
+      marginBottom: 20,
+      fontWeight: 'bold',
+    },
+    input: {
+      width: '100%',
+      marginBottom: 15,
+      backgroundColor: colors.onSecondaryContainer,
+    },
+    button: {
+      width: '100%',
+      marginVertical: 10,
+    },
+    registerButton: {
+      borderWidth: 2,
+    },
+    forgotSection: {
+      marginTop: 20,
+      backgroundColor: colors.background,
+    },
+    forgotText: {
+      color: colors.primary,
+      textAlign: 'center',
+      textDecorationLine: 'underline',
+      marginTop: 20,
+      paddingBottom: 7,
+    },
+  });
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={styles.container}>
+          <View style={styles.topSection}>
+            <Text style={styles.headerText}>DRINK</Text>
+            <Text style={styles.taglineText}>Craft with flair, drink with care!</Text>
+          </View>
+
+          <View style={[styles.middleSection]}>
+            <Text style={styles.logoText}>LOGO</Text>
+            <Text style={styles.loginText}>Login</Text>
+
+            <TextInput
+              onChangeText={(text) => setTestStr(text)}
+              label="Login / Mail"
+              mode="outlined"
+              textColor={colors.primary}
+              style={styles.input}
+            />
+            <TextInput
+              label="Password"
+              mode="outlined"
+              textColor={colors.primary}
+              secureTextEntry={true}
+              style={styles.input}
+            />
+
+            <Button
+              mode="contained-tonal"
+              onPress={() => navigation.navigate('Recipes')}
+              style={styles.button}
+              contentStyle={{ height: 70 }}
+              labelStyle={{ fontWeight: 'bold' }}
+            >
+              LOGIN
+            </Button>
+
+            <Button
+              mode="outlined"
+              onPress={() => navigation.navigate('Recipes')}
+              style={[styles.button, styles.registerButton]}
+              contentStyle={{ height: 50 }}
+              labelStyle={{ fontWeight: 'bold' }}
+            >
+              REGISTER
+            </Button>
+
+            <TouchableOpacity onPress={() => console.log(data)} style={styles.forgotSection}>
+              <Text style={styles.forgotText}>If you forgot your password click here</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginView;
--- apps/DrinkTeamWeb/src/views/Search/SearchView.tsx
+++ apps/DrinkTeamWeb/src/views/Search/SearchView.tsx
@@ -137,6 +137,12 @@ const SearchView = () => {
           keyExtractor={(item) => item.id.toString()}
         />
       )}
+      <TouchableOpacity
+        style={style.addButton}
+        onPress={() => navigation.navigate('AddRecipe')}
+      >
+        <Text style={style.addButtonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -204,6 +210,24 @@ const styles = (colors: MD3Colors) => {
     listItemDuration: {
       color: '#FFFFFF',
       fontSize: 12,
+    },
+    addButton: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      backgroundColor: colors.primary,
+      justifyContent: 'center',
+      alignItems: 'center',
+      elevation: 5,
+    },
+    addButtonText: {
+      color: '#FFFFFF',
+      fontSize: 24,
+      fontWeight: 'bold',
     },
   });
 };
